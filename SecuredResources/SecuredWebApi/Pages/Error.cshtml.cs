d87c8035ad801baae9f2776e35dcaa4d491b32b8
Pouya Panahy <P.Panahy@Rdc.nl>
2021-05-07 16:07:05 +0200

    using .net core application without npm

--- SecuredResources/SecuredWebApi/Pages/Error.cshtml.cs
+++ SecuredResources/SecuredWebApi/Pages/Error.cshtml.cs
@@ -6,18 +6,19 @@ using System.Diagnostics;
 namespace SecuredWebApi.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    [IgnoreAntiforgeryToken]
     public class ErrorModel : PageModel
     {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
         private readonly ILogger<ErrorModel> _logger;
 
         public ErrorModel(ILogger<ErrorModel> logger)
         {
             _logger = logger;
         }
-
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
         public void OnGet()
         {
--- SecuredResources/SecuredWebApi/Program.cs
+++ SecuredResources/SecuredWebApi/Program.cs
@@ -1,9 +1,15 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace SecuredWebApi
 {
-    public static class Program
+    public class Program
     {
         public static void Main(string[] args)
         {
